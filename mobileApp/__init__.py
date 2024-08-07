ffb821f2f1921bafa2d24c7854634491d392fea9
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-08-07 10:02:55 +0200

    Switch the organization

--- mobileApp/__init__.py
+++ mobileApp/__init__.py
@@ -1,11 +1,1 @@
-from kivy_reloader.app import App
-
-
-class MainApp(App):
-    def build(self):
-        from .screens.main_screen import MainScreen
-
-        return MainScreen(name="Main Screen")
-
-
-app = MainApp()
+from .budgetAssistantApp import app
