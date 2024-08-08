c227f83bf628cd2e279d3a2ddc948f549374aa27
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-08-08 15:10:55 +0200

    Actually Fixed the app

--- mobileApp/budgetAssistantApp.py
+++ mobileApp/budgetAssistantApp.py
@@ -3,8 +3,8 @@ from kivy_reloader.app import App
 
 class MainApp(App):
     def build(self):
-
+        from .screens.main_screen import MainScreen
         from .screens.main_manager import MainManager
-        return MainManager()
+        return MainScreen()
 
 app = MainApp()
--- mobileApp/screens/main_manager.py
+++ mobileApp/screens/main_manager.py
@@ -1,15 +1,15 @@
 import os
 
 from kivy.uix.screenmanager import ScreenManager
-
+from kivy.lang import Builder
 from kivy_reloader.utils import load_kv_path
 from .main_screen import *
-
 
 
 class MainManager(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
 
 manager_screen_kv = os.path.join("mobileApp", "screens", "main_manager.kv")
 load_kv_path(manager_screen_kv)
