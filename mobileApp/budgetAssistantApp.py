76b33f4dde5b7e2e3323a9eb1517ea66053f137d
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-10-07 09:46:12 +0200

    initial addition of Expenses Screen

--- mobileApp/budgetAssistantApp.py
+++ mobileApp/budgetAssistantApp.py
@@ -5,6 +5,7 @@ class MainApp(App):
     def build(self):
         from .screens.dashboard_screen.dashboard_screen import DashboardScreen
         from .screens.main_screen.main_screen import MainScreen
+        from .screens.expenses_screen.expenses_screen import ExpensesScreen
         from .screens.main_manager.main_manager import MainManager
         return MainManager()
 
