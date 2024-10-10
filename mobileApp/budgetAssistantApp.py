44b419cc04cadae4401ea97376c9a71502077274
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-10-10 15:07:16 -0300

    🔥 Remove unecessary imports

--- mobileApp/budgetAssistantApp.py
+++ mobileApp/budgetAssistantApp.py
@@ -1,12 +1,9 @@
 from kivy_reloader.app import App
+from .screens.main_manager import MainManager
 
 
 class MainApp(App):
     def build(self):
-        from .screens.dashboard_screen.dashboard_screen import DashboardScreen
-        from .screens.main_screen.main_screen import MainScreen
-        from .screens.expenses_screen.expenses_screen import ExpensesScreen
-        from .screens.main_manager.main_manager import MainManager
         return MainManager()
 
 app = MainApp()
