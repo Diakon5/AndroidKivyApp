98eda263e44c0c429159a4dc55e7631557d4d61d
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-10-10 15:09:49 -0300

    🔥 Removed useless lines

--- mobileApp/screens/main_manager/main_manager.py
+++ mobileApp/screens/main_manager/main_manager.py
@@ -1,14 +1,11 @@
 import os
 
 from kivy.uix.screenmanager import ScreenManager
-from kivy.lang import Builder
 from kivy_reloader.utils import load_kv_path
-from ..main_screen.main_screen import *
 
 
 class MainManager(ScreenManager):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    pass
 
 
 manager_screen_kv = os.path.join("mobileApp", "screens","main_manager", "main_manager.kv")
