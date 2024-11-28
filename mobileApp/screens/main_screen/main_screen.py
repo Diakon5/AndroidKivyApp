f673fba21efd33dcd1effc665744d0ad5c99c129
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-11-28 11:56:49 +0100

    Major flop

--- mobileApp/screens/main_screen/main_screen.py
+++ mobileApp/screens/main_screen/main_screen.py
@@ -1,12 +1,17 @@
 import os
-
+from kivy.factory import Factory
 from kivy.uix.screenmanager import Screen
+from kivy.metrics import dp
 from kivy.properties import ObjectProperty
 from kivy_reloader.utils import load_kv_path
 
 
 class MainScreen(Screen):
     vehicleListScroll = ObjectProperty(None)
+
+    def addNewCar(self):
+        row = Factory.Button()
+        self.vehicleListScroll.add_widget(row)
     def on_enter(self, *args):
         print("MainScreen on_enter")
 
