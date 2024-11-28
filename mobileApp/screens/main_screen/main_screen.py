401bb0bab86a5433d963a2ade3b941c887d4ef98
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-11-28 10:42:33 +0100

    Began adding interactive adding of data

--- mobileApp/screens/main_screen/main_screen.py
+++ mobileApp/screens/main_screen/main_screen.py
@@ -1,11 +1,12 @@
 import os
 
 from kivy.uix.screenmanager import Screen
-
+from kivy.properties import ObjectProperty
 from kivy_reloader.utils import load_kv_path
 
 
 class MainScreen(Screen):
+    vehicleListScroll = ObjectProperty(None)
     def on_enter(self, *args):
         print("MainScreen on_enter")
 
