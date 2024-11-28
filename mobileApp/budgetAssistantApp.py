f092efab05ddded3ca77637c207fd68c85a08a7f
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-11-28 14:48:45 +0100

    finally fix the goddamn database

--- mobileApp/budgetAssistantApp.py
+++ mobileApp/budgetAssistantApp.py
@@ -1,15 +1,21 @@
-from kivy_reloader.app import App
+#from kivy_reloader.app import App
+from kivy.app import App
 from .screens.main_manager import MainManager
 from kivy.utils import platform
 from kivy.cache import Clock
-import aiosqlite as sqlite
+import anysqlite as sqlite
 from functools import partial
 #import sqlite3 as sqlite
 from os import getcwd
 from os.path import join
+import trio
 
 class MainApp(App):
-
+    async def async_run(self, async_lib = "trio"):
+        async with trio.open_nursery() as nursery:
+            self.nursery = nursery # note this
+            await super().async_run('trio')
+            nursery.cancel_scope.cancel()
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.internal_storage_path = join(getcwd(),"debugStorage")
@@ -26,15 +32,6 @@ class MainApp(App):
                                    CREATE TABLE IF NOT EXISTS documents (document_id INTEGER PRIMARY KEY AUTOINCREMENT, document_scan_date DATETIME, document_print_date DATETIME, document_sum_cache DECIMAL);
                                    CREATE TABLE IF NOT EXISTS document_rows (row_id INTEGER PRIMARY KEY AUTOINCREMENT, document_id INTEGER, purchase_category INT, purchase_name TEXT, purchase_price INT, FOREIGN KEY(document_id) REFERENCES documents(document_id));
                                    COMMIT;''')
-    # async def db_init(self):
-        # self.conn = await sqlite.connect(join(self.internal_storage_path,"storage.sqlite"))
-        # cursor = await self.conn.cursor()
-        # 
-        # await cursor.executescript('''BEGIN;
-                                #    CREATE TABLE IF NOT EXISTS managed_vehicles_list (vehicle_id INT PRIMARY KEY AUTO_INCREMENT, vehicle_display_name TEXT);
-                                #    CREATE TABLE IF NOT EXISTS documents (document_id INT PRIMARY KEY AUTO_INCREMENT, document_scan_date DATETIME, document_print_date DATETIME, document_sum_cache DECIMAL);
-                                #    CREATE TABLE IF NOT EXISTS document_rows (row_id INT PRIMARY KEY AUTO_INCREMENT, document_id INT FOREIGN KEY NOT NULL,purchase_category INT FOREIGN KEY NOT NULL, purchase_name TEXT, purchase_price INT)
-                                #    COMMIT''')
     async def db_read(self):
         pass
     async def db_write(self):
