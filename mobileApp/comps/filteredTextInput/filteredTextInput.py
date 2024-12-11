984d2f30a31d96d9d75b74f54e1bd5e116ae1af9
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-12-11 09:14:55 +0100

    Adjust Text Filtering

--- mobileApp/comps/filteredTextInput/filteredTextInput.py
+++ mobileApp/comps/filteredTextInput/filteredTextInput.py
@@ -1,7 +1,7 @@
 from kivy.uix.textinput import TextInput
 import re
 class FilteredTextInput(TextInput):
-    pattern = re.compile("\'\"")
+    pattern = re.compile("\'\"\:\;\`")
     def insert_text(self, substring, from_undo=False):
         
         substring = re.sub(self.pattern,"",substring)
