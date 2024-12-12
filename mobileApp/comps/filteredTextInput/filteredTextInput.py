14ff1ff111f8cf09f44899c54abb85e78b13a616
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-12-12 12:43:37 +0100

    Add simple text validation

--- mobileApp/comps/filteredTextInput/filteredTextInput.py
+++ mobileApp/comps/filteredTextInput/filteredTextInput.py
@@ -1,7 +1,13 @@
 from kivy.uix.textinput import TextInput
+from kivy.properties import BooleanProperty
 import re
 class FilteredTextInput(TextInput):
     pattern = re.compile("\'\"\:\;\`")
+    accept_empty = BooleanProperty(False)
+    def on_text_validate(self):
+        if not self.accept_empty and (len(self.text)==0 or not self.text.isspace()):
+            return False
+        return True
     def insert_text(self, substring, from_undo=False):
         
         substring = re.sub(self.pattern,"",substring)
