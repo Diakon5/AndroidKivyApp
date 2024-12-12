bcb298e7c823b842a5f5258cdfa7e53fd7f3198d
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-12-12 15:47:54 +0100

    start working on document screen

--- mobileApp/screens/documents_screen/documents_screen.py
+++ mobileApp/screens/documents_screen/documents_screen.py
@@ -1,12 +1,30 @@
 import os
-
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ...budgetAssistantApp import MainApp
+from kivy.properties import ListProperty, BooleanProperty
 from kivy.uix.screenmanager import Screen
-
+from kivy.app import App
 from kivy_reloader.utils import load_kv_path
 
 
 class DocumentsScreen(Screen):
+    db_ready = BooleanProperty(None)
+    rows = ListProperty([("id","Loading Data, please wait")])
+    async def list_documents(self, dt=0): ##To future self: ALWAYS TRY EXCEPT YOUR CALLS MORON
+        try:
+            print("TRYING TO LIST")
+            app : MainApp = App.get_running_app() 
+            output = await app.db_read_single_table("documents","*")
+            self.rows = output
+            print(self.rows)
+            print("CONNECTED")
+        except Exception as e:
+            print("Exception", e.args)
     def on_enter(self, *args):
+        app : MainApp = App.get_running_app()
+        if self.db_ready:
+            app.nursery.start_soon(self.list_cars)
         print("DocumentsScreen on_enter")
 
 documents_screen_kv = os.path.join("mobileApp", "screens","documents_screen", "documents_screen.kv")
